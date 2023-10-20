18fd566b800bc96b9614ae7250da31e425052595
sebas1acero <sebas1acero@gmail.com>
2023-10-19 21:13:23 -0500

    cambios2 inicio sesion

--- Js/Script.js
+++ Js/Script.js
@@ -1,4 +1,5 @@
 const boton0 = document.getElementById("registrouno");
+const boton1 = document.getElementById("btnregreso");
 const contenido0 = document.getElementById("contenido0");
 const contenido1 = document.getElementById("contenido1");
 contenido0.style.display = "block";
@@ -6,6 +7,12 @@ contenido1.style.display = "none";
 
 boton0.addEventListener("click", function () {
     contenido0.style.display = "none";
-    contenido1.style.display = "block";
+    contenido1.style.display = "flex";
+
+});
+
+boton1.addEventListener("click", function () {
+    contenido0.style.display = "block";
+    contenido1.style.display = "none";
 
 });
