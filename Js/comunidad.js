1cc250345be028eebdcc27b10ea7f4021cec0ea3
sebas1acero <sebas1acero@gmail.com>
2023-10-23 00:29:16 -0500

    cambios3 Comunidad

--- Js/comunidad.js
+++ Js/comunidad.js
@@ -1,40 +1,93 @@
-const abrirPublicacion = document.getElementById('abrir-publicacion');
-const areaPublicacion = document.getElementById('area-publicacion');
+const abrirPopup = document.getElementById('abrir-popup');
+const cerrarPopup = document.getElementById('cerrar-popup');
+const popup = document.getElementById('popup');
+const publicar = document.getElementById('publicar');
 const textoPublicacion = document.getElementById('texto-publicacion');
 const imagenPublicacion = document.getElementById('imagen-publicacion');
-const publicar = document.getElementById('publicar');
 const contenidoPrincipal = document.getElementById('contenido-principal');
 
-abrirPublicacion.addEventListener('click', () => {
-    areaPublicacion.style.display = 'block';
+// seccion del pop up
+abrirPopup.addEventListener('click', () => {
+    popup.style.display = 'block';
 });
 
+cerrarPopup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+//seccion de publicar
 publicar.addEventListener('click', () => {
-    // Obtener el texto de la publicación
     const texto = textoPublicacion.value;
+    const imagen = imagenPublicacion.files[0];
 
-    // Crear un elemento <div> para mostrar la publicación
-    const publicacion = document.createElement('div');
-    publicacion.className = 'publicacion';
+    //condicion para limitar los caracteres a publicar 
+    if (texto.length > 80) {
+        alert('El texto no puede superar los 50 caracteres.');
+        return;
+    }
+    //seccion para determinar que hacer con el texto y la imagen a publicar 
+    if (texto || imagen) {
 
-    // Agregar el contenido de texto a la publicación
-    const parrafo = document.createElement('p');
-    parrafo.textContent = texto;
-    publicacion.appendChild(parrafo);
+        const nuevaPublicacion = document.createElement('section');
+        nuevaPublicacion.className = 'publicacion';
 
-    // Verificar si se cargó una imagen
-    if (imagenPublicacion.files.length > 0) {
-        // Crear una imagen y mostrarla en la publicación
-        const imagen = document.createElement('img');
-        imagen.src = URL.createObjectURL(imagenPublicacion.files[0]);
-        publicacion.appendChild(imagen);
+        const header = document.createElement('section');
+        header.className = 'publicacion-header';
+
+
+        const imagenPerfil = document.createElement('img');
+        imagenPerfil.className = 'minfoto';
+        imagenPerfil.src = 'img/perfil.png';
+
+        const nombreUsuario = document.createElement('p');
+        nombreUsuario.textContent = 'Nombre de Usuario';
+        nombreUsuario.classList.add('titulopubli');
+
+        // definimos que se publicara en el header de la publicacion 
+        header.appendChild(imagenPerfil);
+        header.appendChild(nombreUsuario);
+        nuevaPublicacion.appendChild(header);
+
+        if (texto) {
+            const parrafo = document.createElement('p');
+            parrafo.className = 'publicacion-texto';
+            parrafo.textContent = texto;
+            nuevaPublicacion.appendChild(parrafo);
+        }
+
+        if (imagen) {
+            const imagenElement = document.createElement('img');
+            imagenElement.src = URL.createObjectURL(imagen);
+            nuevaPublicacion.appendChild(imagenElement);
+            imagenElement.classList.add('fotopublicada');
+        }
+
+        const opciones = document.createElement('section');
+        opciones.className = 'publicacion-opciones';
+
+        const botonMeGusta = document.createElement('button');
+        botonMeGusta.className = 'boton-opcion';
+        botonMeGusta.textContent = 'Me gusta';
+
+        const botonComentar = document.createElement('button');
+        botonComentar.className = 'boton-opcion';
+        botonComentar.textContent = 'Comentar';
+
+        const botonCompartir = document.createElement('button');
+        botonCompartir.className = 'boton-opcion';
+        botonCompartir.textContent = 'Compartir';
+
+
+        opciones.appendChild(botonMeGusta);
+        opciones.appendChild(botonComentar);
+        opciones.appendChild(botonCompartir);
+        nuevaPublicacion.appendChild(opciones);
+        // hace que las publicaciones aparescan de primeras 
+        contenidoPrincipal.insertBefore(nuevaPublicacion, contenidoPrincipal.firstChild);
     }
 
-    // Agregar la publicación al contenido principal
-    contenidoPrincipal.appendChild(publicacion);
-
-    // Limpiar el área de publicación
+    // Restablecer el formulario
     textoPublicacion.value = '';
     imagenPublicacion.value = '';
-    areaPublicacion.style.display = 'none';
+    popup.style.display = 'none';
 });
