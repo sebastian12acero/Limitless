2961b5841f07ece2ab5c3a003549c05f6fb6f11a
sebas1acero <sebas1acero@gmail.com>
2023-11-05 02:35:28 -0500

    cambio-textos-mejora-del boton

--- Js/Responsive.js
+++ Js/Responsive.js
@@ -1,27 +1,18 @@
-// Captura el botón y el menú
-const button = document.querySelector(".btn-custom");
-const dropdownMenu = document.querySelector(".contenedorlista ul");
+const button = document.querySelector(".btn-header");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
-// Variable para controlar el estado del menú
 let isMenuVisible = false;
 
-// Agregar un evento de clic al botón
+
 button.addEventListener("click", () => {
-  if (isMenuVisible) {
-    // Si el menú está visible, ocultarlo
+  if (isMenuVisible) { 
     dropdownMenu.style.display = "none";
     isMenuVisible = false;
   } else {
-    // Si el menú no está visible, mostrarlo
     dropdownMenu.style.display = "block";
     isMenuVisible = true;
   }
+ 
+
 });
 
-// Agregar un evento de redimensionamiento de ventana para ocultar el menú en una cierta anchura
-window.addEventListener("resize", () => {
-  if (window.innerWidth <= 768) { // Ajusta el valor según tus necesidades
-    dropdownMenu.style.display = "none";
-    isMenuVisible = false;
-  }
-});
